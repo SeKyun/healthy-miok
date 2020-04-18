6659202973542ecd95aa6d6b2ba889d062a80be5
Kyun2da <dktnskdktnsk@gmail.com>
2020-04-18 20:54:35 +0900

    feat : 서버 배포 테스트

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,41 @@
-import React from 'react';
-import './App.css';
-import Routes from '../src/components/pages/Route';
+// import React from 'react';
+// import './App.css';
+// import Routes from '../src/components/pages/Route';
 
-function App() {
-  return (
-    <div className="App">
-      <Routes />
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <Routes />
+
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = { users: [] };
+
+  componentDidMount() {
+    fetch('/users')
+      .then((res) => res.json())
+      .then((users) => this.setState({ users }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Users</h1>
+        <ul>
+          {this.state.users.map((user) => (
+            <li key={user.id}> {user.username} </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
