b0ae82a7e1c9f6254cd09638c6720cdd4574df19
Kyun2da <dktnskdktnsk@gmail.com>
2020-04-26 01:39:24 +0900

    feat : 에러페이지 css 수정

--- frontend/src/components/organisms/NotWelcome.js
+++ frontend/src/components/organisms/NotWelcome.js
@@ -1,7 +1,21 @@
 import React from 'react';
-
+import './NotWelcome.scss';
+import { Button } from 'antd';
+import history from '../pages/history';
 const NotWelcome = () => {
-  return <div>허가된 사용자가 아닙니다. 돌아가주세요..</div>;
+  return (
+    <div className="NotWelcome">
+      <div>허가된 사용자가 아닙니다. 돌아가주세요..</div>
+      <Button
+        size="large"
+        onClick={() => {
+          history.push('/');
+        }}
+      >
+        돌아가기
+      </Button>
+    </div>
+  );
 };
 
 export default NotWelcome;
