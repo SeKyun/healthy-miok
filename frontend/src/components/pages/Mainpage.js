0ef9cdbda354578027d67855af1c16842ed53070
Kyun2da <dktnskdktnsk@gmail.com>
2020-04-26 18:54:59 +0900

    feat: 탭 추가

--- frontend/src/components/pages/Mainpage.js
+++ frontend/src/components/pages/Mainpage.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Mainpage.scss';
 import { Layout } from 'antd';
 import Antmenu from '../organisms/Antmenu.js';
 const { Header, Footer, Content } = Layout;
 const Mainpage = () => {
+  const [menuIdx, setMenuIdx] = useState(1);
   return (
     <Layout>
       <Header>
-        <Antmenu />
+        <Antmenu func={setMenuIdx} />
       </Header>
       <Layout>
-        <Content>Content</Content>
+        <Content>{menuIdx}</Content>
       </Layout>
       <Footer>Footer</Footer>
     </Layout>
