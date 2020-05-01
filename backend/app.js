3cddc74fec992d54f8ab9884c6441473490f1113
Dodie Joo <dodi258@naver.com>
2020-05-01 23:41:11 +0900

    Revert "feat: db 연결, greeting api 추가"
    This reverts commit cb45636a31e73b688553adc23282a60e0ced48f8.

--- backend/app.js
+++ backend/app.js
@@ -17,12 +17,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-// possible error point 
-app.use(express.static(path.join(__dirname, "../frontend/public")));
-
-// possible error point
-app.use('/api', indexRouter);
+app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,16 +1,9 @@
-let express = require('express');
-let router = express.Router();
-let path = require("path"); 
-
-let greeting = require("./greeting"); 
+var express = require('express');
+var router = express.Router();
 
 /* GET home page. */
-// possible error point 
 router.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname, "../../frontend/public", "index.html")); 
-  // res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express' });
 });
-
-router.use("/greeting", greeting); 
 
 module.exports = router;
