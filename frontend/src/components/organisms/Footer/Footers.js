0e704c17b461b468f57692b3a73042af748324dd
Kyun2Da <dktnskdktnsk@gmail.com>
2020-05-20 00:06:12 +0900

    feat : Footer 쪽 정보 버튼 modal 추가

--- frontend/src/components/organisms/Footer/Footers.js
+++ frontend/src/components/organisms/Footer/Footers.js
@@ -1,12 +1,49 @@
 import React from 'react';
 import './Footers.scss';
-import { Button } from 'antd';
+import { Modal, Button } from 'antd';
 const Footers = () => {
+  const [state, setState] = React.useState(false);
+  const showModal = () => {
+    setState(true);
+  };
+  const handleOk = () => {
+    setState(false);
+  };
+
   return (
     <div className="Footer">
       <div className="question">문의 : healthymiok@gmail.com</div>
       <div className="maker">
-        <Button>정보</Button>
+        <Modal
+          title="정보"
+          visible={state}
+          onCancel={handleOk}
+          centered
+          okText={'확인'}
+          footer={null}
+        >
+          주세인 :{' '}
+          <a
+            href="https://github.com/dodi258"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github
+          </a>{' '}
+          <br />
+          허균 :{' '}
+          <a
+            href="https://github.com/Kyun2da"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github
+          </a>{' '}
+          <br />
+          디자인 도움 : 채희원
+          <br />
+        </Modal>
+        <Button onClick={showModal}>정보</Button>
         <div>Backend : Sein Joo</div>
         <div>Frontend : Kyun Heo</div>
       </div>
