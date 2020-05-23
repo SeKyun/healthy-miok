d1a6a22eb9088f9ac5da137c524c2c83d747346e
Kyun Heo <kyun2dot@gmail.com>
2020-05-23 23:47:07 +0900

    feat: 혈당 등록 쪽 수정기능 빼고 완료, 목록 데이터 받아오는 폼 완성

--- frontend/src/components/organisms/home/bloodSugar/history/history.js
+++ frontend/src/components/organisms/home/bloodSugar/history/history.js
@@ -10,9 +10,25 @@ const { RangePicker } = DatePicker;
 const History = () => {
   const dateFormat = 'YYYY/MM/DD';
   const date = new Date();
+  const [dataSource, setDataSource] = React.useState([
+    {
+      key: 1,
+      month: 5,
+      day: 23,
+      '아침 식전': 300,
+      '아침 식후': 200,
+      '점심 식전': 200,
+      '점심 식후': 200,
+      '저녁 식전': 200,
+      '저녁 식후': 200,
+      '취침 전': 200,
+      새벽: 200,
+      기타: 200,
+    },
+  ]);
   const getData = async () => {
     const response = await axios.get('http://miok.site:3001/api/blood-sugar');
-    console.log(response);
+    console.log(response.data.result);
   };
   return (
     <div>
@@ -28,26 +44,26 @@ const History = () => {
           조회
         </Button>
       </div>
-      <Table bordered={true}>
+      <Table dataSource={dataSource} bordered={true}>
         <ColumnGroup title="날짜">
-          <Column title="월" dataIndex="firstName" key="firstName" />
-          <Column title="일" dataIndex="lastName" key="lastName" />
+          <Column title="월" dataIndex="month" key="month" />
+          <Column title="일" dataIndex="day" key="day" />
         </ColumnGroup>
         <ColumnGroup title="아침">
-          <Column title="공복" dataIndex="age" key="age" />
-          <Column title="식후2시간" dataIndex="address" key="address" />
+          <Column title="식전" dataIndex="아침 식전" key="아침 식전" />
+          <Column title="식후2시간" dataIndex="아침 식후" key="아침 식후" />
         </ColumnGroup>
         <ColumnGroup title="점심">
-          <Column title="식전" dataIndex="age" key="age" />
-          <Column title="식후2시간" dataIndex="address" key="address" />
+          <Column title="식전" dataIndex="점심 식전" key="점심 식전" />
+          <Column title="식후2시간" dataIndex="점심 식후" key="점심 식후" />
         </ColumnGroup>
         <ColumnGroup title="저녁">
-          <Column title="식전" dataIndex="age" key="age" />
-          <Column title="식후2시간" dataIndex="address" key="address" />
+          <Column title="식전" dataIndex="저녁 식전" key="저녁 식전" />
+          <Column title="식후2시간" dataIndex="저녁 식후" key="저녁 식후" />
         </ColumnGroup>
-        <Column title="취침전" dataIndex="age" key="age" />
-        <Column title="새벽" dataIndex="age" key="age" />
-        <Column title="기타" dataIndex="age" key="age" />
+        <Column title="취침전" dataIndex="취침 전" key="취침 전" />
+        <Column title="새벽" dataIndex="새벽" key="새벽" />
+        <Column title="기타" dataIndex="기타" key="기타" />
       </Table>
     </div>
   );
