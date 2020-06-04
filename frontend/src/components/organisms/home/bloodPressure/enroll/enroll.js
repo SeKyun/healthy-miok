ce0c7fe3a8abb783a4fffc51f240ce0654d0ab52
Kyun2Da <dktnskdktnsk@gmail.com>
2020-06-05 00:08:53 +0900

    style: 혈압 등록페이지 스타일 수정

--- frontend/src/components/organisms/home/bloodPressure/enroll/enroll.js
+++ frontend/src/components/organisms/home/bloodPressure/enroll/enroll.js
@@ -1,21 +1,34 @@
 import React from 'react';
 import { Form, DatePicker, TimePicker, InputNumber, Input, Button } from 'antd';
 import './enroll.scss';
+import moment from 'moment';
 
 const { TextArea } = Input;
 const enroll = () => {
   const formItemLayout = {
-    labelCol: { span: 6 },
+    labelCol: { span: 9 },
     wrapperCol: { span: 13 },
   };
   return (
     <div className="bloodPressureEnroll">
       <Form {...formItemLayout}>
-        <Form.Item wrapperCol={{ offset: 12 }}>
-          <DatePicker size="large" />
+        <Form.Item wrapperCol={{ offset: 11 }}>
+          <DatePicker
+            size="large"
+            style={{
+              margin: '5% 0px 8% 0px',
+              fontSize: '40px',
+            }}
+            defaultValue={moment(new Date(), 'YYYY-MM-DD')}
+            className="datePic"
+          />
         </Form.Item>
         <Form.Item label="시간">
-          <TimePicker size="large" />
+          <TimePicker
+            size="large"
+            format={'h:mm a'}
+            defaultValue={moment(new Date(), 'HH:MM')}
+          />
         </Form.Item>
         <Form.Item label="혈압 수축">
           <InputNumber size="large" />
@@ -27,19 +40,24 @@ const enroll = () => {
         </Form.Item>
         <Form.Item label="심박수">
           <InputNumber size="large" />
+          bpm
         </Form.Item>
-        <Form.Item
-          label="메모"
-          labelCol={{ span: 6 }}
-          wrapperCol={{ span: 14 }}
-        >
+        <Form.Item label="메모" wrapperCol={{ span: 13 }}>
           <TextArea
             placeholder="쓰고싶은 말을 써주세요."
-            style={{ width: '60%' }}
+            style={{ width: '60%', fontSize: '30px' }}
           />
         </Form.Item>
-        <Form.Item colon={false} wrapperCol={{ span: 12, offset: 12 }}>
-          <Button type="primary" htmlType="submit">
+        <Form.Item colon={false} wrapperCol={{ span: 13, offset: 12 }}>
+          <Button
+            type="primary"
+            htmlType="submit"
+            style={{
+              margin: '5% 0px 0px 0px',
+              fontSize: '30px',
+              height: '100%',
+            }}
+          >
             등록
           </Button>
         </Form.Item>
