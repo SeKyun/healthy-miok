728b59e015e70ed06cfedb524241c1e1d4bedc57
Dodie Joo <dodi258@naver.com>
2020-06-21 15:39:43 +0900

    feat: 인슐린 타입 지속성(long) 혹은 속효성(short)만 불러오기
    api 부르기
    api/type-insulin/:type
    (type 은 'long' 혹은 'short')

--- backend/routes/type_insulin/index.js
+++ backend/routes/type_insulin/index.js
@@ -15,4 +15,6 @@ router.delete('/', controller.delete_all);
 /** DELETE the type of insulin data with its name */
 router.delete('/:name', controller.delete_type); 
 
+/** GET type of insulin by its type */
+router.get('/:type', controller.get_bytype); 
 module.exports = router; 
