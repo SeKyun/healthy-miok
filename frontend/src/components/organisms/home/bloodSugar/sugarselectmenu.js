6d506d1c05ced3560df0f9c42cbe198c90645703
Kyun Heo <kyun2dot@gmail.com>
2020-06-23 22:15:38 +0900

    refactor : 혈당쪽 리펙토링

--- frontend/src/components/organisms/home/bloodSugar/sugarselectmenu.js
+++ frontend/src/components/organisms/home/bloodSugar/sugarselectmenu.js
@@ -1,18 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Graph from './graph/graph';
 import History from './history/history';
 import Enroll from './enroll/enroll';
+import Error from '../../../pages/Error/Error';
 
-const SugarselectMenu = (props) => {
-  const { menuIdx } = props;
-
+const SugarselectMenu = ({ menuIdx }) => {
   if (menuIdx === 0) {
     return <Enroll />;
-  } else if (menuIdx === 1) {
+  }
+  if (menuIdx === 1) {
     return <History />;
-  } else if (menuIdx === 2) {
+  }
+  if (menuIdx === 2) {
     return <Graph />;
   }
+  return <Error />;
+};
+
+SugarselectMenu.propTypes = {
+  menuIdx: PropTypes.number.isRequired,
 };
 
 export default SugarselectMenu;
