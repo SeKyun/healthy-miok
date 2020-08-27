d5034f9a2c5611b58c0ae72f79a3e83153621e96
Dodie Joo <dodi258@naver.com>
2020-08-27 23:58:02 +0900

    feat: config 설정 - 비번 숨기기

--- backend/config/key.js
+++ backend/config/key.js
@@ -1,9 +1,17 @@
 
 //possible error point 
-if (process.env.NODE_ENV === 'developmet') {
-    // local에서 개발 중일 떄 
-    module.exports = require('./dev'); 
-} else {
-    // 서버에서 배포 중일 때 
-    module.exports = require('./prod'); 
+// if (process.env.NODE_ENV === 'developmet') {
+//     // local에서 개발 중일 떄 
+//     module.exports = require('./dev'); 
+// } else {
+//     // 서버에서 배포 중일 때 
+//     module.exports = require('./prod'); 
+// }
+
+module.exports = {
+    mysqlHOST: 'healthy-miok-db.css640snysar.ap-northeast-2.rds.amazonaws.com', 
+    mysqlPORT: 3306,
+    mysqlUSER: 'admin',
+    mysqlPASSWORD: 'jigunmiok1118', 
+    mysqlDATABASE: 'healthy_miok'
 }
