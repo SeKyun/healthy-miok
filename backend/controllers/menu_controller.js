838ec7e8214b233027ff4095441594e6d56a6934
dodi258 <dodi258@naver.com>
2020-11-04 18:11:35 +0900

    test: kakao api 이미지 검색 추가

--- backend/controllers/menu_controller.js
+++ backend/controllers/menu_controller.js
@@ -1,6 +1,7 @@
 const menu_list = require('../data/menu.json');
 const res_handler = require('../library/status_handler');
 const url = require('url');
+const kakao_api = require('../library/kakao_open_api_image'); 
 
 exports.get_menu = function (req, res) {
 	let queryData = url.parse(req.url, true).query;
@@ -14,34 +15,37 @@ exports.get_menu = function (req, res) {
 	switch(country) {
 		case 'korea':
 			menues = menu_list.korea.food_list[type]; 
-			result.img_url = menu_list.korea.image_url; 
+			// result.img_url = menu_list.korea.image_url; 
 			break; 
 		case 'boonsick':
 			menues = menu_list.boonsick.food_list[type]; 
-			result.img_url = menu_list.boonsick.image_url; 
+			// result.img_url = menu_list.boonsick.image_url; 
 			break; 
 		case 'ameria':
 			menues = menu_list.america.food_list[type]; 
-			result.img_url = menu_list.america.image_url; 
+			// result.img_url = menu_list.america.image_url; 
 			break; 
 		case 'europe':
 			menues = menu_list.europe.food_list[type]; 
-			result.img_url = menu_list.europe.image_url; 
+			// result.img_url = menu_list.europe.image_url; 
 			break; 
 		case 'china':
 			menues = menu_list.china.food_list[type]; 
-			result.img_url = menu_list.china.image_url; 
+			// result.img_url = menu_list.china.image_url; 
 			break; 
 		case 'japan':
 			menues = menu_list.japan.food_list[type];
-			result.img_url = menu_list.japan.image_url; 
+			// result.img_url = menu_list.japan.image_url; 
 			break; 
 	}
 	 
 	index = Math.floor(Math.random() * MENU_CNT); 
 	
 	result.menu = menues[index];
-	console.log(result + " " + index);
+	console.log("*********" + kakao_api.get_img_url(result.menu)); 
+	result.img_url = kakao_api.get_img_url(result.menu); 
+
+	console.log(result.menu + " " + index);
 
 	return res_handler.sendSuccess(result, 200, res, 'menu'); 
 
