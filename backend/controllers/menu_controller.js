ab48a3eaa24b4de4b1783ea1d5c998e7281edbd9
dodi258 <dodi258@naver.com>
2020-11-04 15:08:56 +0900

    feat: menu.json에 img_url 추가

--- backend/controllers/menu_controller.js
+++ backend/controllers/menu_controller.js
@@ -9,32 +9,38 @@ exports.get_menu = function (req, res) {
 	const MENU_CNT = 6;
 	let menues = ""; 
 	let index = 0; 
-	let result = 0; 
+	let result = {}; 
 
 	switch(country) {
 		case 'korea':
-			menues = menu_list.korea[type]; 
+			menues = menu_list.korea.food_list[type]; 
+			result.img_url = menu_list.korea.image_url; 
 			break; 
 		case 'boonsick':
-			menues = menu_list.boonsick[type]; 
+			menues = menu_list.boonsick.food_list[type]; 
+			result.img_url = menu_list.boonsick.image_url; 
 			break; 
 		case 'ameria':
-			menues = menu_list.america[type]; 
+			menues = menu_list.america.food_list[type]; 
+			result.img_url = menu_list.america.image_url; 
 			break; 
 		case 'europe':
-			menues = menu_list.europe[type]; 
+			menues = menu_list.europe.food_list[type]; 
+			result.img_url = menu_list.europe.image_url; 
 			break; 
 		case 'china':
-			menues = menu_list.china[type]; 
+			menues = menu_list.china.food_list[type]; 
+			result.img_url = menu_list.china.image_url; 
 			break; 
 		case 'japan':
-			menues = menu_list.japan[type];
+			menues = menu_list.japan.food_list[type];
+			result.img_url = menu_list.japan.image_url; 
 			break; 
 	}
 	 
 	index = Math.floor(Math.random() * MENU_CNT); 
 	
-	result = menues[index]; 
+	result.menu = menues[index];
 	console.log(result + " " + index);
 
 	return res_handler.sendSuccess(result, 200, res, 'menu'); 
