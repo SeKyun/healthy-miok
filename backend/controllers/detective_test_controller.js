3a112937a678009ec4cb5ecfea509e237a20af44
dodi258 <dodi258@naver.com>
2020-11-09 18:47:12 +0900

    test: detective_test 수정

--- backend/controllers/detective_test_controller.js
+++ backend/controllers/detective_test_controller.js
@@ -14,17 +14,21 @@ exports.get_result = function (req, res) {
     let q5 = Number(queryData.q5);
 
     let q_total = q1 + q2 + q3 + q4 + q5; 
-    let result = {
-        ans_cnt: q_total,
+    let r = {
+        cnt: q_total,
     };
     if (q_total < 2) {
-        result.ability = '보통의 추리력';
+        r.comment = '보통 사람의 추리력을 지녔어요!';
+        r.img_url = 'https://i.pinimg.com/originals/87/7a/45/877a456a618b506059121e309d45c015.png'
+
     } else if (q_total < 4) {
-        result.ability = '예리한 추리력'; 
+        r.comment = '예리한 추리력을 지녔어요! 당신.. 추리 좀 하는 놈인가?';
+        r.img_url = 'https://i.pinimg.com/564x/81/35/22/8135224c100c7fa7b35b5142e4fa3d17.jpg' 
     } else if (q_total >= 4) {
-        result.ability = '아주 뛰어난 추리력';
+        r.comment = '아주 뛰어난 추리력을 지녔어요!! ';
+        r.img_url = 'https://i.pinimg.com/564x/73/1a/7f/731a7f0e2038600129c9e2c812f9f00e.jpg'
     }
 
-    return res_handler.sendSuccess(result, 200, res, "detective_test"); 
+    return res_handler.sendSuccess(r, 200, res, "detective_test"); 
 
 }
