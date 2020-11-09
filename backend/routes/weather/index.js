b08417326ad5a4fa4944ea7bb2c0c596cb30c8c9
dodi258 <dodi258@naver.com>
2020-11-09 11:43:08 +0900

    feat: 대기상태 사진 불러오는 기능 추가

--- backend/routes/weather/index.js
+++ backend/routes/weather/index.js
@@ -19,6 +19,16 @@ router.get('/', async function (req, res, next) {
     // 검색이 가능해졌을 경우. 
     else {
         console.log("NAVER RESULT!!!!!!!!!!:" + naver_result); 
+        let smog_status = naver_result[0].smog_status;
+        
+        if (smog_status === '좋음') {
+            naver_result[0].smog_img_url = "https://i.pinimg.com/originals/57/89/2d/57892d8f88d5503f5dfc7532ff2642a6.png"; 
+        } else if (smog_status === '보통') {
+            naver_result[0].smog_img_url = "https://i.pinimg.com/originals/85/c7/29/85c729ec1093b130df2c208d60d54571.png";
+        } else {
+            naver_result[0].smog_img_url = "https://i.pinimg.com/originals/64/42/35/644235f76829abe9d3e87825ae766049.png"; 
+        }
+
         const google_result = await controller.google_weather_controller.getHtml(naver_result[0].city_name);
         naver_result[0].img_src = "http://kweather.co.kr/" + google_result;  
         res.json(naver_result); 
