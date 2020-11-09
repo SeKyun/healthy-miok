f3de9de34d8c66c8db487da15465549486838a5a
dodi258 <dodi258@naver.com>
2020-11-09 11:21:04 +0900

    feat: 검색 결과가 없을 경우를 추가.

--- backend/routes/weather/index.js
+++ backend/routes/weather/index.js
@@ -9,10 +9,21 @@ router.get('/', async function (req, res, next) {
     let key_word = queryData.key_word;
     
     const naver_result = await controller.naver_weather_controller.getHtml(key_word); 
-    console.log("NAVER RESULT!!!!!!!!!!:" + naver_result); 
-    const google_result = await controller.google_weather_controller.getHtml(naver_result[0].city_name);
-    naver_result[0].img_src = "http://kweather.co.kr/" + google_result;  
-    res.json(naver_result); 
+    console.log("naver_result: " + naver_result); 
+    // 검색이 안되었을 경우에
+    if (! naver_result[0]) {
+        res.status(404).send(
+            naver_result
+        )
+    }
+    // 검색이 가능해졌을 경우. 
+    else {
+        console.log("NAVER RESULT!!!!!!!!!!:" + naver_result); 
+        const google_result = await controller.google_weather_controller.getHtml(naver_result[0].city_name);
+        naver_result[0].img_src = "http://kweather.co.kr/" + google_result;  
+        res.json(naver_result); 
+    }
+    
 }); 
 
 module.exports = router; 
