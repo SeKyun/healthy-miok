0f801c13a7544d1283dd4e8859317cbd52d8e4e1
dodi258 <dodi258@naver.com>
2020-11-10 12:13:45 +0900

    test: 날씨 api 수정

--- backend/controllers/weather_controller.js
+++ backend/controllers/weather_controller.js
@@ -31,30 +31,35 @@ exports.naver_weather_controller = function() {
                 micro_s: $(this).children("div.sub_info").children("div.detail_box").children("dl.indicator").children("dd").eq(1).text().slice(-2),
                 ozone_v: $(this).children("div.sub_info").children("div.detail_box").children("dl.indicator").children("dd").children("span.num").eq(2).text(),
                 ozone_s: $(this).children("div.sub_info").children("div.detail_box").children("dl.indicator").children("dd").eq(2).text().slice(-2),
-                city_name: $("#main_pack > section.sc_new.cs_weather._weather > div > div.api_cs_wrap > div.blind > h3").text(),
+                city_name: $("#main_pack > div.sc.cs_weather_global._oversea_weather > div:nth-child(3) > div.weather_box > div.weather_area._mainArea > div.sort_box._areaSelectLayer > div > div > span > em").text(),
             }
         });
-        let cast_status = ulList[0].cast_txt.split(",")[0]; 
-        if (cast_status.includes('맑음')){
-            ulList[0].img_url = "https://i.pinimg.com/originals/a5/99/60/a59960d92f95e5c1a343ef917a50ac55.jpg"
-        } else if (cast_status.includes('구름')) {
-            ulList[0].img_url = "https://i.pinimg.com/originals/85/9d/50/859d50b6af304e90480dc1822b096ed6.jpg";
-        } else if (cast_status.includes('흐림')) {
-            ulList[0].img_url = "https://i.pinimg.com/originals/57/c2/be/57c2beae9d7cafb97fccec0c55128e10.jpg";
-        } else if (cast_status.includes('비')) {
-            ulList[0].img_url = "https://i.pinimg.com/originals/d3/bb/3e/d3bb3e4691aa6195fcae51754f8e6dd8.jpg"; 
-        } else if (cast_status.includes('흐린')) {
-            ulList[0].img_url = "https://i.pinimg.com/originals/15/ee/08/15ee08e3318acc82e4352eed7b917cda.jpg"; 
-        } else if (cast_status.includes('안개')) {
-            ulList[0].img_url = "https://i.pinimg.com/originals/ea/45/bc/ea45bcb6f91c8167b3abd306d01585a1.jpg"; 
-        } else if (cast_status.includes('우박')) {
-            ulList[0].img_url = "https://i.pinimg.com/originals/bd/cd/e1/bdcde1292eadd3bba1f98556962bea92.jpg"; 
-        } else if (cast_status.includes('황사')) {
-            ulList[0].img_url = "https://i.pinimg.com/originals/29/c3/c6/29c3c6b8fe913b54cf8a507c9154d43a.jpg"; 
-        } else if (cast_status.includes('번개') || cast_status.includes('뇌우')) {
-            ulList[0].img_url = "https://i.pinimg.com/originals/72/4a/9a/724a9a8ed0cfc18bff99f170fe426f92.jpg"; 
-        } else {
-            ulList[0].img_url = "https://i.pinimg.com/originals/08/33/46/083346e944bf3658a6acd5e4d2e99706.jpg"; 
+        if (! ulList[0].city_name) {
+            ulList[0].city_name = "검색한 지역의 날씨"; 
+        }
+        if (ulList[0].cast_txt) {
+            let cast_status = ulList[0].cast_txt.split(",")[0]; 
+            if (cast_status.includes('맑음')){
+                ulList[0].img_url = "https://i.pinimg.com/originals/a5/99/60/a59960d92f95e5c1a343ef917a50ac55.jpg"
+            } else if (cast_status.includes('구름')) {
+                ulList[0].img_url = "https://i.pinimg.com/originals/85/9d/50/859d50b6af304e90480dc1822b096ed6.jpg";
+            } else if (cast_status.includes('흐림')) {
+                ulList[0].img_url = "https://i.pinimg.com/originals/57/c2/be/57c2beae9d7cafb97fccec0c55128e10.jpg";
+            } else if (cast_status.includes('비')) {
+                ulList[0].img_url = "https://i.pinimg.com/originals/d3/bb/3e/d3bb3e4691aa6195fcae51754f8e6dd8.jpg"; 
+            } else if (cast_status.includes('흐린')) {
+                ulList[0].img_url = "https://i.pinimg.com/originals/15/ee/08/15ee08e3318acc82e4352eed7b917cda.jpg"; 
+            } else if (cast_status.includes('안개')) {
+                ulList[0].img_url = "https://i.pinimg.com/originals/ea/45/bc/ea45bcb6f91c8167b3abd306d01585a1.jpg"; 
+            } else if (cast_status.includes('우박')) {
+                ulList[0].img_url = "https://i.pinimg.com/originals/bd/cd/e1/bdcde1292eadd3bba1f98556962bea92.jpg"; 
+            } else if (cast_status.includes('황사')) {
+                ulList[0].img_url = "https://i.pinimg.com/originals/29/c3/c6/29c3c6b8fe913b54cf8a507c9154d43a.jpg"; 
+            } else if (cast_status.includes('번개') || cast_status.includes('뇌우')) {
+                ulList[0].img_url = "https://i.pinimg.com/originals/72/4a/9a/724a9a8ed0cfc18bff99f170fe426f92.jpg"; 
+            } else {
+                ulList[0].img_url = "https://i.pinimg.com/originals/08/33/46/083346e944bf3658a6acd5e4d2e99706.jpg"; 
+            }
         }
 
         const data = ulList.filter(n => n.todaytemp); 
